d7e860566b699ef14dd70baf0ef9e8e4bda74050
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-03-30 01:38:05 -0700

    Minor changes

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function doLogin () {
     let tbPassword = document.getElementById("password").value;
     console.log(tbUserName, tbPassword);
     if (tbUserName=='admin@admin.com'&& tbPassword=='123456') {
-      window.location.href="login.html";
+      window.location.href="welcome.html";
     } else {
       return "Wrong Password";
   }

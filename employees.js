9ff10f8b2192bcbd08e8ea87f77fda21376f4475
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-03-30 13:46:21 -0700

    Final changes and minor styling

--- employees.js
+++ employees.js
@@ -1,212 +1,220 @@
-let employeetable = document.getElementById("myemployeetable");
+let employee = [
+  {
+   "name": "Hal Goodman",
+   "idnumber": "43578029",
+   "storenumber": "2",
+   "employeetype": "Employee",
+   "picture": "https://www.minneapolisheadshots.com/gallery/main/blackandwhitebusinessportrait.jpg",
+  },
+  {
+   "name": "Weyenshet Fantaye",
+   "idnumber": "78300272",
+   "storenumber": "1",
+   "employeetype": "Manager",
+   "picture": "https://www.minneapolisheadshots.com/gallery/main/blackandwhitebusinessportrait.jpg",
+  },
+  {
+   "name": "Gracie Christopher",
+   "idnumber": "67892373",
+   "storenumber": "5",
+   "employeetype": "Manager",
+   "picture": "https://www.minneapolisheadshots.com/gallery/main/blackandwhitebusinessportrait.jpg",
 
-function myFunction() {
-  // Declare variables 
-  var input, filter, employeetable, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  employeetable = document.getElementById("myemployeetable");
-  tr = employeetable.getElementsByTagName("tr");
+  },
+  {
+   "name": "Kristy Harold",
+   "idnumber": "89897236",
+   "storenumber": "10",
+   "employeetype": "Employee",
+   "picture": "https://www.minneapolisheadshots.com/gallery/main/blackandwhitebusinessportrait.jpg",
 
-  // Loop through all employeetable rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
-  let books = [
-    {
-        "title": "Things Fall Apart",
-        "author": "Chinua Achebe",
-        "genre": "Fiction",
-        "country": "Nigeria",
-        "language": "English",
-        "length": "209",
-        "year": "1958",
-        "isbn": "978-7-1735-1972-0",
-        "condition": "Fair"
-       },
-       {
-        "title": "Fairy tales",
-        "author": "Hans Christian Andersen",
-        "genre": "Fiction",
-        "country": "Denmark",
-        "language": "Danish",
-        "length": "784",
-        "year": "1836",
-        "isbn": "978-7-1735-1972-1",
-        "condition": "Fair"
-       },
-       {
-        "title": "The Divine Comedy",
-        "author": "Dante Alighieri",
-        "genre": "Fiction",
-        "country": "Italy",
-        "language": "Italian",
-        "length": "928",
-        "year": "1315",
-        "isbn": "0-7067-4636-8",
-        "condition": "Poor"
-       },
-       {
-        "title": "The Epic Of Gilgamesh",
-        "author": "Unknown",
-        "genre": "Fiction",
-        "country": "Sumer and Akkadian Empire",
-        "language": "Akkadian",
-        "length": "160",
-        "year": 1700,
-        "isbn": "0-3933-0529-5",
-        "condition": "Good"
-       },
-       {
-        "title": "The Book Of Job",
-        "author": "Unknown",
-        "genre": "Fiction",
-        "country": "Achaemenid Empire",
-        "language": "Hebrew",
-        "length": "176",
-        "year": 600,
-        "isbn": "0-6845-1134-7",
-        "condition": "Great"
-       },
-       {
-        "title": "The Magic Mountain",
-        "author": "Thomas Mann",
-        "genre": "PsychologicalFiction",
-        "country": "Germany",
-        "language": "German",
-        "length": "720",
-        "year": "1924",
-        "isbn": "0-3283-5716-2",
-        "condition": "Good"
-       },
-       {
-        "title": "Moby Dick",
-        "author": "Herman Melville",
-        "genre": "PsychologicalFiction",
-        "country": "United States",
-        "language": "English",
-        "length": "378",
-        "year": "1851",
-        "isbn": "0-8091-0004-5",
-        "condition": "Great"
-       },
-       {
-        "title": "Essays",
-        "author": "Michel de Montaigne",
-        "genre": "PsychologicalFiction",
-        "country": "France",
-        "language": "French",
-        "length": "404",
-        "year": "1595",
-        "isbn": "0-6872-0495-X",
-        "condition": "Poor"
-       },
-       {
-        "title": "History",
-        "author": "Elsa Morante",
-        "genre": "PsychologicalFiction",
-        "country": "Italy",
-        "language": "Italian",
-        "length": "600",
-        "year": "1974",
-        "isbn": "978-8-4517-5443-7",
-        "condition": "Great"
-       },
-       {
-        "title": "Beloved",
-        "author": "Toni Morrison",
-        "genre": "PsychologicalFiction",
-        "country": "United States",
-        "language": "English",
-        "length": "321",
-        "year": "1987",
-        "isbn": "978-1-1177-7373-5",
-        "condition": "Good"
-       },
-       {
-        "title": "The Tale of Genji",
-        "author": "Murasaki Shikibu",
-        "genre": "PsychologicalFiction",
-        "country": "Japan",
-        "language": "Japanese",
-        "length": "1360",
-        "year": "1006",
-        "isbn": "978-6-9789-4583-3",
-        "condition": "Fair"
-       },
-       {
-        "title": "The Man Without Qualities",
-        "author": "Robert Musil",
-        "genre": "PsychologicalFiction",
-        "country": "Austria",
-        "language": "German",
-        "length": "365",
-        "year": "1931",
-        "isbn": "0-9124-2177-0",
-        "condition": "Poor"
-       },
-       {
-        "title": "Lolita",
-        "author": "Vladimir Nabokov",
-        "genre": "PsychologicalFiction",
-        "country": "Russia\/United States",
-        "language": "English",
-        "length": "317",
-        "year": "1955",
-        "isbn": "978-0-6179-6131-4",
-        "condition": "Poor"
-       },
-       {
-        "title": "Nineteen Eighty-Four",
-        "author": "George Orwell",
-        "genre": "Political Fiction",
-        "country": "United Kingdom",
-        "language": "English",
-        "length": "272",
-        "year": "1949",
-        "isbn": "978-1-1960-6341-3",
-        "condition": "Great"
-       },
-    ]
+  },
+  {
+   "name": "Matthew Lopez",
+   "idnumber": "78204892",
+   "storenumber": "4",
+   "employeetype": "Employee"
+  },
+  {
+   "name": "David Goliath",
+   "idnumber": "72909021",
+   "storenumber": "7",
+   "employeetype": "Manager"
+  },
+  {
+   "name": "Daisy Smith",
+   "idnumber": "78902398",
+   "storenumber": "3",
+   "employeetype": "Employee"
+  },
+  {
+   "name": "Chigozie Obioma",
+   "idnumber": "98922356",
+   "storenumber": "9",
+   "employeetype": "Manager"
+  },
+  {
+   "name": "Dawn Summners",
+   "idnumber": "87923012",
+   "storenumber": "5",
+   "employeetype": "Employee"
+  },
+  {
+   "name": "Carrie Washington",
+   "idnumber": "82225347",
+   "storenumber": "6",
+   "employeetype": "Employee"
+  },
+  {
+   "name": "Helen Johnson",
+   "idnumber": "92368210",
+   "storenumber": "8",
+   "employeetype": "Employee"
+  },
+]
+     
 
-    function populateemployeetablewitharray(myemployeetable) {
-      // get the employeetable to add rows to
+    analyzeemployees(employee);
+  
+    function analyzeemployees(data) {
+      let employeecards = document
+        .getElementById("employee-cards")
+        .getElementsByTagName("div")[0];
+    
+        employeecards.innerHTML = "";
+    
+      for (let i = 0; i < employee.length; i++) {
+        let personcard = `<div class="card">
+                        <p class="name-text">${employee[i].name}</p>
+                        <p class="id-text">ID Number:  ${employee[i].idnumber}</p>
+                        <p class="store-text">Store Number: ${employee[i].storenumber}</p>
+                        <p class="employeetype-text">Employee Type: ${employee[i].employeetype}</p>
+                        <p><img src="${employee[i].picture}"</p>
 
-      // cycle through the array for each of the presidents
-      for (var i = 0; i < books.length; ++i) {
-        // keep a reference to an individual president object
-        let book = books[i];
-    
-        // create a row element to append cells to
-        var row = document.createElement('tr');
-    
-        // properties of the array elements
-        var properties = ['title', 'author', 'genre','country','language','length','year','isbn','condition'];
-    
-        // append each one of them to the row in question, in order
-        for (var j = 0; j < properties.length; ++j) {
-          // create new data cell for names
-          var cell = document.createElement('td');
-    
-          // set name of property using bracket notation (properties[j] is a string,
-          // which can be used to access properties of president)
-          cell.innerHTML = book[properties[j]];
-    
-          // add to end of the row
-          row.appendChild(cell);
-        }
-    
-        // add new row to employeetable
-        employeetable.appendChild(row);
+                        <p><button id="btnDelete" >Delete</button>
+                           <button id="btnEdit" onclick="editRow()">Edit</button>
+                        </p>
+                        </div>
+                      `;
+                    
+                     employeecards.innerHTML += personcard;
       }
     }
-    populateemployeetablewitharray(books);
+    
+    const employeecards = document
+      .getElementById("employee-cards")
+      .getElementsByTagName("div")[0];
+    
+    function addRow() {
+      
+        const name = document.getElementById("name").value;
+        const idnumber = document.getElementById("idnumber").value;
+        const storenumber = document.getElementById("storenumber").value;
+        const employeetype = document.getElementById("employeetype").value;
+        const picture = document.getElementById("imageurl").value;
+
+  
+        console.log(name, idnumber, storenumber, employeetype);
+    
+        const newEmployee = {
+          name: name,
+          idnumber: idnumber,
+          storenumber: storenumber,
+          employeetype: employeetype,
+          picture: picture  
+        };
+    
+        employee.push(newEmployee);
+        
+        analyzeemployees(employee);
+      
+    }
+    
+    // const btnEdit = document.getElementById("btnEdit");
+    // console.log(btnEdit);
+    // The Delete Row function
+    addEventListener("click", (e) => {
+      if (e.target.id === "btnDelete") {
+        console.log(e.target.parentElement.parentElement);
+        const indexToRemove = e.target.parentElement.parentElement.rowIndex - 1;
+        employee.splice(indexToRemove, 1);
+      }
+      analyzeemployees(employee);
+    });
+
+    // The Edit Row function
+  
+  const btnEdit = document.getElementById("my-button");
+  console.log(btnEdit);
+  const btnSave = document.getElementById("btnSave");
+  console.log(btnSave);
+  
+  var mycards = document.getElementById("employee-cards");
+  var rIndex;
+  function editRow() {
+    for (var i = 1; i < mycards.div.length; i++) {
+      mycards.div[i].onclick = function () {
+        rIndex = this.rowIndex;
+        console.log(rIndex);
+  
+
+        document.getElementById("name").value = this.cells[0].innerHTML;
+        document.getElementById("idnumber").value = this.cells[1].innerHTML;
+        document.getElementById("storenumber").value = this.cells[2].innerHTML;
+        document.getElementById("employeetype").value = this.cells[3].innerHTML;
+        document.getElementById("imageurl").value = this.cells[4].innerHTML;
+
+      };
+  
+      document.getElementById("my-button").style.display = "none";
+      document.getElementById("btnSave").style.display = "block";
+    }
+  }
+  
+    btnSave.addEventListener("click", (event) => {
+    console.log(rIndex);
+    console.log(employee);
+  
+    const name = document.getElementById("name").value;
+    const idnumber = document.getElementById("idnumber").value;
+    const storenumber = document.getElementById("storenumber").value;
+    const employeetype = document.getElementById("employeetype").value;
+
+  
+    for (var i = 0; i < employee.length; i++) {
+      if (i === rIndex - 1) {
+        employee[i].name = name;
+        employee[i].idnumber = idnumber;
+        employee[i].storenumber = storenumber;
+        employee[i].employeetype = employeetype;
+
+      }
+    }
+  
+    event.preventDefault();
+    document.getElementById("my-button").style.display = "block";
+    document.getElementById("btnSave").style.display = "none";
+  
+    console.log(name, idnumber, storenumber, employeetype);
+    analyzeemployees(employee);
+  });
+  
+  // Function for clearing the inputs after the add button was clicked on
+  
+  let btnClear = document.getElementById("my-button");
+  let inputs = document.querySelectorAll("input");
+  
+  btnClear.addEventListener("click", () => {
+    inputs.forEach((input) => (input.value = ""));
+  });
+  
+  btnSave.addEventListener("click", () => {
+    inputs.forEach((input) => (input.value = ""));
+  });
+  
+  //This function makes sure that the user has enterted all the required information.
+  
+
+
+  
+
